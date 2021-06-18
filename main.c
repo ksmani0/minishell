7fe27ba40c2957e81e2df5a73f2bef6fcf740581
sanghpar <k1990121@gmail.com>
2021-06-19 01:21:32 +0200

    add parse

--- main.c
+++ main.c
@@ -4,18 +4,20 @@ char **get_input()
 {
 	char	*tmp;
 	char	**commands;
+	char	**tmp2;
 	int		i;
 
 	tmp = ft_strdup("");
 	get_next_line(STDIN_FILENO, &tmp);
 	commands = ft_split(tmp, ';');
+	tmp2 = commands;
 	while (*commands)
 	{
 		*commands = ft_trim(*commands, ' ');
 		//*commands = convert_quotes(*commands);
 		commands++;
 	}
-	return (commands);
+	return (tmp2);
 }
 
 //"echo $haha\"$haha\"'haha'";
@@ -32,7 +34,8 @@ int main(int argc, char **argv, char **envp)
 	{
 		write(0, "$", 1);
 		input = get_input();
-		parse_execute(input);
+		if (*input)
+			parse_execute(input);
 	}
 	return (0);
 }
--- srcs/parse_execute.c
+++ srcs/parse_execute.c
@@ -14,33 +14,80 @@ void	reset_buf(char *k)
 	}
 }
 
-t_cmd	*make_cmd(char *command)
+void	norm_by_pass(char *buf, char **command)
+{
+	if (**command == '|' || **command == '<')
+	{
+		*buf = **command;
+		buf = buf + 1;
+		*command = *command + 1;
+	}
+	else if (**command == '>')
+	{
+		if (**(command + 1) == '>')
+		{
+			*buf = '>';
+			*(buf + 1) = '>';
+			*command = *command + 2;
+		}
+		else
+		{
+			*buf = **command;
+			buf = buf + 1;
+			*command = *command + 1;
+		}
+	}
+}
+
+char	*get_token(char **command)
 {
 	char	buf[9000];
+	int		i;
+
+	reset_buf(buf);
+	i = 0;
+	if (**command == '|' || **command == '<' || **command == '>')
+	{
+		norm_by_pass(buf, command);
+		return (ft_strdup(buf));
+	}
+	while (**command)
+	{
+		if (**command == ' ' || **command == '\t' || **command == '\n')
+		{
+			*command = *command + 1;
+			break ;
+		}
+		buf[i++] = **command;
+		*command = *command + 1;
+	}
+	if (*buf == 0)
+		return (0);
+	return (ft_strdup(buf));
+}
+
+t_cmd	*make_cmd(char *command)
+{
 	char	*argv;
 	t_cmd	*tmp;
 	
-	reset_buf(buf);
 	tmp = (t_cmd *)malloc(sizeof(t_cmd));
 	tmp->cmd = 0;
 	tmp->argv_list = 0;
 	while (*command)
 	{
-		argv = get_argv();
-		if (tmp->cmd == 0)
-			tmp->cmd = argv;
-		else
-			ft_lstadd_back(tmp->argv_list, ft_lstnew((void *)argv));
+		argv = get_token(&command);
+		if (argv != 0)
+			ft_lstadd_back(&(tmp->argv_list), ft_lstnew((void *)argv));
 	}
+	return (tmp);
 }
 
 void	parse_execute(char **commands)
 {
 	t_cmd *cmd;
-
-	while (*commands)
-	{
-		cmd = make_cmd(*commands);
-		execute_cmd(cmd);
-	}
+	
+	cmd = make_cmd(*commands);
+	int k = 4;
+	//execute_cmd(cmd);
 }
