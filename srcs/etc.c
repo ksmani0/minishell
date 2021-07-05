b0d57557c853a4f57f78e297b2f7115b94b383f6
eno <k1990121@gmail.com>
2021-07-05 19:40:18 +0200

    aa

--- srcs/etc.c
+++ srcs/etc.c
@@ -1,18 +1,54 @@
 #include "minishell.h"
 
-static bool     check_executable(struct stat sb)
+static void     check_executable(struct stat sb, int i, t_cmd *c_list, char *buf)
 {
-    if (sb.st_mode & S_IXUSR)
-        return (true);
+    if (i != 0)
+    {
+        printf("bash: %s: command not found\n", c_list->cmd->content);
+		g_data->ret = 127;
+        return ;
+    }
+    else if (S_ISDIR(sb.st_mode))
+    {
+        printf("bash: %s: is a directory\n", c_list->cmd->content);
+		g_data->ret = 126;
+        return ;
+    }
     else
     {
-        
+        if (sb.st_mode & S_IXUSR)
+            execute_etc(c_list, buf);
+        else
+            return ;
     }
 }
 
 static  void    execute_etc(t_cmd *c_list, char *buf)
 {
+    int		status;
+	pid_t	pid;
 
+	pid = fork();
+	if (pid < 0)
+		exit(1);
+	if (pid == 0)
+	{
+		set_rd(c_list->r_list);
+		if (execve(buf, argv, envp) < 0)
+        {
+            printf("bash: %s: %s\n", c_list->cmd->content, strerror(errno));
+		    if (errno == 13)
+			    exit(126);
+		    else if (errno == 2)
+			    exit(127);
+        }
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+		if (WIFEXITED(status))
+			g_data->ret = WEXITSTATUS(status);
+	}
 }
 
 void            etc(t_cmd *c_list)
@@ -24,15 +60,14 @@ void            etc(t_cmd *c_list)
 
     cmd = c_list->cmd->content;
     if (ft_strchr(cmd, '/') == 0)
-        stat(c_list->cmd->content, &sb);
+        i = stat(c_list->cmd->content, &sb);
     else
     {
         getcwd(buf, 5000);
         i = ft_strlen(buf);
         buf[i] = '/';
         ft_strlcpy((buf + i), cmd, ft_strlen(cmd) + 1);
-        stat(buf, &sb);
+        i = stat(buf, &sb);
     }
-    if (check_executable(sb))
-        execute_etc(c_list, buf);
+    check_executable(sb, i, c_list, buf);
 }
